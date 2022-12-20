e6a819f9ca837f0b0a18d9fdc354252ac530435c
David Koppstein <koppstein@ie-freiburg.mpg.de>
2022-12-20 14:59:37 +0100

    add mpi commands to run.sh

--- run.sh
+++ run.sh
@@ -3,3 +3,4 @@ eval "$(conda shell.bash hook)"
 conda activate snakemake-cia
 mkdir -p workspace/
 snakemake --directory workspace/ --configfile config/config.yaml -s CIAassembly_pipeline -j 16 all
+#snakemake --profile ~/.config/mpi-ie-slurm --directory workspace/ --configfile config/config-mpi.yaml -s CIAassembly_pipeline -j 16 all
