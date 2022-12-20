31433125ea9c3da2a4d033b86949cd4f0ab5504d
David Koppstein <koppstein@ie-freiburg.mpg.de>
2022-12-20 12:00:56 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 eval "$(conda shell.bash hook)"
-conda activate snakemake
+conda activate snakemake-cia
 mkdir -p workspace/
-snakemake --profile ~/.config/mpi-ie-slurm --directory workspace/ --configfile config/config-mpi.yaml -s CIAassembly_pipeline -j 16 all
+snakemake --directory workspace/ --configfile config/config.yaml -s CIAassembly_pipeline -j 16 all
